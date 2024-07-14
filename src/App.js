51e02f67c72c357fd4cb627b70b10f1ca3103b22
realBijan <bijan.taheri@iu-study.org>
2024-07-14 14:58:13 +0200

    Auth0 Implementation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-// src/App.js
 import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
 import { Routes, Route } from 'react-router-dom';
 import TicketListPage from './pages/TicketListPage';
 import CreateTicket from './pages/CreateTicket';
@@ -7,16 +7,37 @@ import EditTicket from './pages/EditTicket';
 import Header from './components/Header'; // Importieren Sie die Header-Komponente
 
 function App() {
+  const { isLoading, isAuthenticated, error, user, loginWithRedirect, logout } = useAuth0();
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>Oops... {error.message}</div>;
+  }
+
   return (
     <div className="App">
-      <Header /> {/* Fügen Sie die Header-Komponente hinzu */}
-      <main>
-        <Routes>
-          <Route path="/" element={<TicketListPage />} />
-          <Route path="/create" element={<CreateTicket />} />
-          <Route path="/edit/:id" element={<EditTicket />} />
-        </Routes>
-      </main>
+      {isAuthenticated ? (
+        <>
+          <Header /> {/* Fügen Sie die Header-Komponente hinzu */}
+          <main>
+            <Routes>
+              <Route path="/" element={<TicketListPage />} />
+              <Route path="/create" element={<CreateTicket />} />
+              <Route path="/edit/:id" element={<EditTicket />} />
+            </Routes>
+          </main>
+          <div>
+            Hello {user.name}{' '}
+            <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
+              Log out
+            </button>
+          </div>
+        </>
+      ) : (
+        <button onClick={() => loginWithRedirect()}>Log in</button>
+      )}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
 import './index.css';
 import App from './App';
-import { UserProvider } from './UserContext';  // Überprüfen Sie den Pfad
+import { UserProvider } from './UserContext';  // Pfad überprüfen
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <UserProvider>
-    <Router>
-      <App />
-    </Router>
-  </UserProvider>
-);
+const rootElement = document.getElementById('root'); // Verwenden Sie 'root', wie es im HTML-Dokument angegeben ist
+
+if (rootElement) {
+  const root = createRoot(rootElement);
+
+  root.render(
+    <Auth0Provider
+      domain="dev-isef-project.eu.auth0.com"
+      clientId="87fezgW4VeyUVO2iiBPKZASk15Ah5kIi"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <UserProvider>
+        <Router>
+          <App />
+        </Router>
+      </UserProvider>
+    </Auth0Provider>
+  );
+} else {
+  console.error("Target container 'root' is not a DOM element.");
+}
